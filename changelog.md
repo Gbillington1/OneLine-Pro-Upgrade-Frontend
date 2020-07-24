9b8580d5b2ed95f45fb11150c66b3dcef992c53b
Graham Billington <grahamhbillington@gmail.com>
2020-07-24 00:10:02 -0400

    initial commit

---
+++ changelog.md
@@ -0,0 +1,11 @@
+# Changelog
+#### All changes made after 07/21/2020 will be documented in this file.
+
+## [07/22/2020]
+### Added
+ - The project to GitHub
+ - changelog
+ - added `cors` and `mysql` packages
+ - mysql database connection
+
+[07/22/2020]: https://github.com/Gbillington1/OneLine-Pro-Upgrade/compare/92614aa..HEAD
---
+++ client.js
@@ -0,0 +1,24 @@
+// define stripe object
+const stripe = Stripe('pk_test_51H4YHDAfoYJSaxkzN699e0wYkizVOu8gTyllICcuwTm0OP3L8dQbeLy2Qz49CsJ8J7PVh6vN4E0JWN0X6qjZTXzD00EL3qzueD')
+
+// define checkout button
+var checkoutButton = document.getElementById('checkout-button');
+
+// fetch the sessionId from the backend
+var response = fetch('http://localhost:4242/id').then(function(response) {
+  return response.json();
+}).then(function(responseJson) {
+  var sessionId = responseJson.session_id;
+  checkoutButton.addEventListener('click', function() {
+    // redirect user to checkout
+    stripe.redirectToCheckout({
+      sessionId: sessionId
+    }).then(function (result) {
+
+      // If `redirectToCheckout` fails due to a browser or network
+      // error, display the localized error message to your customer
+      // using `result.error.message`.
+      console.error(result.error.message)
+    });
+  });
+});
